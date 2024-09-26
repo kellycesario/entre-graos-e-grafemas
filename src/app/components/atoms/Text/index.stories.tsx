08fff070a4066a3229be5315b3e01ad64b254ad0
kellycesario <kellycesario@gmail.com>
2024-09-25 23:07:38 -0300

    feat: text

---
+++ src/app/components/atoms/Text/index.stories.tsx
@@ -0,0 +1,28 @@
+import { Text } from './index'
+import {
+  alignmentArgTypes,
+  colorArgTypes,
+  weightArgTypes
+} from '@/storybook/argTypes'
+
+export default {
+  title: 'Atoms/Text',
+  component: Text,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'padded',
+  },
+  args: {
+    align: 'center',
+    color: 'dark-gray',
+    children: 'Text example',
+    weight: '600',
+  },
+  argTypes: {
+    ...alignmentArgTypes,
+    ...colorArgTypes,
+    ...weightArgTypes
+  },
+}
+
+export const Default = {}

e957fe0ce254d45e8707f72bf74af6bc9d1aaf20
kellycesario <kellycesario@gmail.com>
2024-10-02 23:03:55 -0300

    docs: update icon .stories

--- src/app/components/ions/Icon/index.stories.tsx
+++ src/app/components/ions/Icon/index.stories.tsx
@@ -1,5 +1,5 @@
 import { Icon } from './index'
-import { IconBook } from "@tabler/icons-react"
+import { IconBook } from '@tabler/icons-react'
 
 export default {
   title: 'Ions/Icon',
@@ -12,8 +12,15 @@ export default {
     Icon: IconBook,
     color: 'red',
     size: 40,
+    stroke: 2,
   },
-
+  argTypes: {
+    Icon: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 }
 
 export const Default = {}

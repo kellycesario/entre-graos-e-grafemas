f9f5d87e5e13b870a474a9cac709cbc1197db0ed
kellycesario <kellycesario@gmail.com>
2024-10-21 23:29:51 -0300

    feat: add ProjectsCTA component in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ProjectsCTA } from '@/organisms/ProjectsCTA'
 import { Hero } from '@/organisms/Hero'
 
 export default function Home() {
@@ -8,6 +9,17 @@ export default function Home() {
         text="Navegue comigo pelas interfaces da linguística e saboreie cada ramificaçãocom um café"
         video="/videos/coffee-05.mp4"
         hasBreadcrumb={false}
+      />
+      <ProjectsCTA
+        alt=""
+        image="/images/eLinC/elinc.jpg"
+        title="Conheça o projeto eLinC"
+        text="O grupo explora construtos teóricos da Gramática Gerativa e da Linguística Cognitiva de forma colaborativa"
+        firstButtonLabel="Mais sobre o eLinC"
+        secondButtonLabel="Visite o site oficial"
+        firstButtonHref="/elinc"
+        secondButtonHref=""
+        direction="row-reverse"
       />
     </main>
   )

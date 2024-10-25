68e563df9191a3d60b3e63c0d27de514cab7c02f
kellycesario <kellycesario@gmail.com>
2024-10-25 17:14:15 -0300

    feat: create social icons component

---
+++ src/components/molecules/SocialIcons/index.stories.tsx
@@ -0,0 +1,8 @@
+import { SocialIcons } from './index'
+
+export default {
+  title: 'Molecules/SocialIcons',
+  component: SocialIcons,
+}
+
+export const Default = {}
---
+++ src/components/molecules/SocialIcons/index.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+import {
+  IconBrandInstagram,
+  IconBrandYoutube,
+  IconBrandLinkedin,
+  IconBrandFacebook,
+} from '@tabler/icons-react'
+import styles from './styles.module.scss'
+
+const socialLinks = [
+  {
+    href: 'https://instagram.com',
+    icon: IconBrandInstagram,
+    aria: 'Instagram',
+  },
+  { href: 'https://linkedin.com', icon: IconBrandLinkedin, aria: 'Linkedin' },
+  { href: 'https://facebook.com', icon: IconBrandFacebook, aria: 'Facebook' },
+  { href: 'https://youtube.com', icon: IconBrandYoutube, aria: 'Youtube' },
+]
+
+export const SocialIcons = () => {
+  return (
+    <nav className={styles.socialIcons} aria-label="redes sociais">
+      <ul className={styles.socialIcons__list}>
+        {socialLinks.map(({ href, icon: Icon, aria }) => (
+          <li key={aria} className={styles.socialIcons__item}>
+            <Link
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label={aria}
+              className={styles.socialIcons__link}
+            >
+              <Icon aria-hidden="true" color="white" />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}

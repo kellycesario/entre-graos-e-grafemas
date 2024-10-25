d9de6d048ca51b9412ec18be17a62b0bc9025ead
kellycesario <kellycesario@gmail.com>
2024-10-25 19:34:56 -0300

    feat: create 'more projects' component

---
+++ src/components/organisms/MoreProjects/index.stories.tsx
@@ -0,0 +1,12 @@
+import { MoreProjects } from './index'
+
+export default {
+  title: 'Organisms/MoreProjects',
+  component: MoreProjects,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+}
+
+export const Default = {}
---
+++ src/components/organisms/MoreProjects/index.tsx
@@ -0,0 +1,36 @@
+'use client'
+import { usePathname } from 'next/navigation'
+import { Heading } from '@/atoms/Heading'
+import { Text } from '@/atoms/Text'
+import { CardCTA } from '@/molecules/CardCTA'
+import projects from '@/data/projects.json'
+import styles from './styles.module.scss'
+
+export const MoreProjects = () => {
+  const pathname = usePathname()
+
+  return (
+    <section aria-labelledby="Demais projetos" className={styles.projects}>
+      <Heading level="2" color="cordovan" weight="700" id="Demais projetos">
+        Entre um gole e outro
+      </Heading>
+      <Text>
+        Enquanto você toma um café, continue a jornada pelos demais tópicos do
+        site
+      </Text>
+
+      <div className={styles.projects__container}>
+        {projects
+          .filter(({ link }) => link !== pathname)
+          .map(({ image, projectName, link }) => (
+            <CardCTA
+              image={image}
+              projectName={projectName}
+              link={link}
+              key={projectName}
+            />
+          ))}
+      </div>
+    </section>
+  )
+}

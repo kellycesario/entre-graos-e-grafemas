a9a688ce568222b30f41431453d508e22914af4f
kellycesario <kellycesario@gmail.com>
2024-10-27 20:48:59 -0300

    feat: first part of the card partner component

---
+++ src/components/molecules/CardPartner/index.stories.tsx
@@ -0,0 +1,26 @@
+import { CardPartner } from './index'
+
+export default {
+  title: 'Molecules/CardPartner',
+  component: CardPartner,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    image:
+      '/images/books/1.webp',
+    name: 'eLinC',
+    role: 'Gerativismo de Chomsky: a teoria que transformou a linguística',
+    description: 'Neste artigo, vamos explorar o gerativismo de Noam Chomsky, que mudou a forma como entendemos a linguagem',
+  },
+  argTypes: {
+    link: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+}
+
+export const Default = {}
---
+++ src/components/molecules/CardPartner/index.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import styles from './styles.module.scss'
+
+interface CardProps {
+  name?: string
+  role?: string
+  description?: string
+  image?: string
+}
+
+export const CardPartner = ({ name, role, description, image }: CardProps) => {
+  return (
+    <article className={styles.card}>
+      {image && (
+        <Image
+          src={image}
+          alt=""
+          width={500}
+          height={600}
+          className={styles.card__image}
+        />
+    )}
+      <div className={styles.card__container}>
+        <div className={styles.card__heading}>
+          <p className={styles.card__name}>{name}</p>
+          <p className={styles.card__role}>{role}</p>
+        </div>
+        <p className={styles.card__description}>{description}</p>
+      </div>
+    </article>
+  )
+}

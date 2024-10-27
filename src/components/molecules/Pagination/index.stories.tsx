e261cfc3e60ca34709e6efff921c73a263db49e2
kellycesario <kellycesario@gmail.com>
2024-10-27 18:05:31 -0300

    feat: create pagination component

---
+++ src/components/molecules/Pagination/index.stories.tsx
@@ -0,0 +1,12 @@
+import { Pagination } from './index'
+
+export default {
+  title: 'Molecules/Pagination',
+  component: Pagination,
+  args: {
+    totalPages: 6,
+    currentPage: 3,
+  },
+}
+
+export const Default = {}
---
+++ src/components/molecules/Pagination/index.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { IconArrowRight, IconArrowLeft } from '@tabler/icons-react'
+import styles from './styles.module.scss'
+
+interface PaginationProps {
+  totalPages: number
+  currentPage: number
+  onPageChange: (page: number) => void
+}
+
+export const Pagination = ({
+  totalPages,
+  currentPage,
+  onPageChange,
+}: PaginationProps) => {
+  const handlePageChange = (newPage: number) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      onPageChange(newPage)
+    }
+  }
+
+  const isPreviousDisabled = currentPage === 1
+  const isNextDisabled = currentPage === totalPages
+
+  const previousIconFill = isPreviousDisabled ? '#5C5C5C' : '#FFFFFF'
+  const nextIconFill = isNextDisabled ? '#5C5C5C' : '#FFFFFF'
+
+  return (
+    <div className={styles.pagination}>
+      <button
+        onClick={() => handlePageChange(currentPage - 1)}
+        aria-label=""
+        className={`${styles.pagination__button} ${
+          isPreviousDisabled ? styles.disabled : styles.active
+        }`}
+        disabled={isPreviousDisabled}
+      >
+        <IconArrowLeft size={32} color={previousIconFill} stroke={1} />
+      </button>
+
+      <div
+        aria-label={`${currentPage} of ${totalPages}`}
+        className={styles.pagination__current}
+      >
+        {currentPage}
+      </div>
+
+      <button
+        onClick={() => handlePageChange(currentPage + 1)}
+        aria-label=""
+        className={`${styles.pagination__button} ${
+          isNextDisabled ? styles.disabled : styles.active
+        }`}
+        disabled={isNextDisabled}
+      >
+        <IconArrowRight size={32} color={nextIconFill} stroke={1} />
+      </button>
+    </div>
+  )
+}

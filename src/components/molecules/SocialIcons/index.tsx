51601f34f91e04a81bfc2be54bbfaa012883d3ff
kellycesario <kellycesario@gmail.com>
2024-11-06 22:18:16 -0300

    feat: make the background color of svg icons dynamic

--- src/components/molecules/SocialIcons/index.tsx
+++ src/components/molecules/SocialIcons/index.tsx
@@ -7,6 +7,10 @@ import {
 } from '@tabler/icons-react'
 import styles from './styles.module.scss'
 
+interface SocialIconsProps {
+  backgroundColor?: string
+}
+
 const socialLinks = [
   {
     href: 'https://instagram.com',
@@ -18,12 +22,16 @@ const socialLinks = [
   { href: 'https://youtube.com', icon: IconBrandYoutube, aria: 'Youtube' },
 ]
 
-export const SocialIcons = () => {
+export const SocialIcons = ({ backgroundColor }: SocialIconsProps) => {
+  const itemStyle = [
+    styles[`socialIcons__item`],
+    styles[`socialIcons__item--${backgroundColor}`],
+  ].join(' ')
   return (
     <nav className={styles.socialIcons} aria-label="Redes Sociais">
       <ul className={styles.socialIcons__list}>
         {socialLinks.map(({ href, icon: Icon, aria }) => (
-          <li key={aria} className={styles.socialIcons__item}>
+          <li key={aria} className={itemStyle}>
             <Link
               href={href}
               target="_blank"

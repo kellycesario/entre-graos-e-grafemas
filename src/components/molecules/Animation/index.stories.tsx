722a2c0fe43598c5e52b7647c02353bd76c490e0
kellycesario <kellycesario@gmail.com>
2024-11-12 21:42:29 -0300

    fest: create animation component with temporary data

---
+++ src/components/molecules/Animation/index.stories.tsx
@@ -0,0 +1,13 @@
+import { Animation } from './index'
+
+export default {
+  title: 'Molecules/Animation',
+  component: Animation,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  args: {
+  },
+}
+
+export const Default = {}
---
+++ src/components/molecules/Animation/index.tsx
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from 'uuid'
+import Image from 'next/image'
+import styles from './styles.module.scss'
+import columnsData from '@/data/columnsData.json'
+
+export const Animation = () => {
+  return (
+    <div className={styles.animation}>
+      {columnsData.map((column, columnIndex) => (
+        <div
+          key={uuidv4()}
+          className={[
+            styles.animation__columns,
+            styles[`animation__column${columnIndex + 1}`],
+          ].join(' ')}
+        >
+          {column.map((image) => (
+            <Image
+              key={image.alt}
+              src={image.src}
+              alt={image.alt}
+              className={styles.animation__image}
+              width={168}
+              height={214}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}

e9ee06a836570ff7e71c12cf1cb1d85680ca3107
kellycesario <kellycesario@gmail.com>
2024-12-06 19:41:25 -0300

    feat: update card partner

--- src/components/molecules/CardPartner/index.tsx
+++ src/components/molecules/CardPartner/index.tsx
@@ -1,16 +1,53 @@
 import Image from 'next/image'
+import { v4 as uuidv4 } from 'uuid'
+import { Icon as CustomIcon } from '@/ions/Icon'
+import {
+  IconBrandInstagram,
+  IconBrandYoutube,
+  IconBrandLinkedin,
+  IconBrandFacebook,
+} from '@tabler/icons-react'
 import styles from './styles.module.scss'
-
 interface CardProps {
+  icons?: { Icon: string; link: string }[]
   name?: string
   role?: string
   description?: string
   image?: string
 }
 
-export const CardPartner = ({ name, role, description, image }: CardProps) => {
+const availableIcons = {
+  Instagram: IconBrandInstagram,
+  Youtube: IconBrandYoutube,
+  Linkedin: IconBrandLinkedin,
+  Facebook: IconBrandFacebook,
+}
+
+export const CardPartner = ({
+  name,
+  role,
+  description,
+  image,
+  icons,
+}: CardProps) => {
+  const id = uuidv4()
+
+  const iconsToRender = icons?.map(({ Icon, link }) => {
+    const IconComponent = availableIcons[Icon as keyof typeof availableIcons]
+
+    const ariaLabel = `${Icon}`
+
+    if (!IconComponent) return null
+
+    return (
+      <a href={link} key={link} aria-label={ariaLabel}>
+        <CustomIcon Icon={IconComponent} color="#1c1c1c" size={24} />
+      </a>
+    )
+  })
+
   return (
-    <article className={styles.card}>
+    <article className={styles.card} aria-labelledby={id}>
       {image && (
         <Image
           src={image}
@@ -22,10 +59,13 @@ export const CardPartner = ({ name, role, description, image }: CardProps) => {
       )}
       <div className={styles.card__container}>
         <div className={styles.card__heading}>
-          <p className={styles.card__name}>{name}</p>
+          <p className={styles.card__name} id={id}>
+            {name}
+          </p>
           <p className={styles.card__role}>{role}</p>
         </div>
         <p className={styles.card__description}>{description}</p>
+        <div className={styles.card__icons}>{iconsToRender}</div>
       </div>
     </article>
   )

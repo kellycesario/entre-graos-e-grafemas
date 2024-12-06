f057df3790d0aac60c8f059cf9ea46991f28c322
kellycesario <kellycesario@gmail.com>
2024-12-06 19:41:58 -0300

    feat: use swipper in partners wrapper

--- src/components/organisms/PartnersWrapper/index.tsx
+++ src/components/organisms/PartnersWrapper/index.tsx
@@ -1,7 +1,13 @@
+'use client'
 import { v4 as uuidv4 } from 'uuid'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Autoplay } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/pagination'
 import { Heading } from '@/atoms/Heading'
 import { Text } from '@/atoms/Text'
 import { CardPartner } from '@/molecules/CardPartner'
+import partnerCategories from '@/data/partnerCategories.json'
 import styles from './styles.module.scss'
 
 interface PartnersWrapperProps {
@@ -10,36 +16,81 @@ interface PartnersWrapperProps {
 }
 
 export const PartnersWrapper = ({ title, text }: PartnersWrapperProps) => {
-  const id = uuidv4()
+  const sectionId = uuidv4()
 
   return (
-    <section aria-labelledby={id} className={styles.partners}>
+    <section className={styles.partners} aria-labelledby={sectionId}>
       <div className={styles.partners__heading}>
-        <Heading color="cordovan" level="2" weight="700" id={id}>
+        <Heading color="cordovan" level="2" weight="700" id={sectionId}>
           {title}
         </Heading>
         <Text>{text}</Text>
       </div>
-      <div className={styles.partners__cards}>
-        <CardPartner
-          name="Nome"
-          role="Papel"
-          description="Descrição"
-          image="https://plus.unsplash.com/premium_photo-1675074446586-c59729d5056d?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
-        <CardPartner
-          name="Nome"
-          role="Papel"
-          description="Descrição"
-          image="https://plus.unsplash.com/premium_photo-1675074446586-c59729d5056d?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
-        <CardPartner
-          name="Nome"
-          role="Papel"
-          description="Descrição"
-          image="https://plus.unsplash.com/premium_photo-1675074446586-c59729d5056d?q=80&w=1335&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
-      </div>
+      {partnerCategories.map(({ category, partners }) => {
+        const categoryId = uuidv4()
+        return (
+          <Swiper
+            className={styles.partners__swiper}
+            aria-labelledby={categoryId}
+            style={
+              {
+                '--swiper-pagination-color': '#8c4949',
+              } as React.CSSProperties
+            }
+            slidesPerView={1}
+            spaceBetween={10}
+            pagination={{
+              clickable: true,
+            }}
+            autoplay={{
+              delay: 8000,
+              disableOnInteraction: true,
+            }}
+            breakpoints={{
+              834: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+              },
+              1200: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+              },
+              1440: {
+                slidesPerView: 4,
+                spaceBetween: 30,
+              },
+            }}
+            modules={[Pagination, Autoplay]}
+            key={categoryId}
+          >
+            <Heading
+              color="cordovan"
+              level="3"
+              weight="600"
+              className={styles.partners__category}
+              id={categoryId}
+            >
+              {category}
+            </Heading>
+            <div>
+              {partners.map((partner) => (
+                <SwiperSlide
+                  key={partner.name}
+                  className={styles.partners__cards}
+                >
+                  <CardPartner
+                    name={partner.name}
+                    role={partner.role}
+                    description={partner.description}
+                    image={partner.image}
+                    icons={partner.icons}
+                  />
+                </SwiperSlide>
+              ))}
+            </div>
+          </Swiper>
+        )
+      })}
     </section>
   )
 }

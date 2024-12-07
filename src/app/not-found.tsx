0473126c6bfb2fb7fb54d956d596e4feca1721f4
kellycesario <kellycesario@gmail.com>
2024-12-06 22:50:33 -0300

    404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,17 @@
+import Image from 'next/image'
+import styles from './not-found.module.scss'
+import { Button } from '@/atoms/Button'
+import { Text } from '@/atoms/Text'
+
 export default function NotFoundPage() {
-  return <div></div>
+  return (
+    <div className={styles.error404}>
+      <Text>Parece que a página que você buscou não existe.</Text>
+      <Text>Que tal pegar um cafézinho e tentar novamente?</Text>
+
+      <Image src="/images/404.png" alt="" width={471} height={318.88} />
+      <Button href="/" label="Voltar para o início" variant="primary" />
+      <p></p>
+    </div>
+  )
 }

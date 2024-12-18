a649870b0a90705f72882283bfb0ec37b82d8474
kellycesario <kellycesario@gmail.com>
2024-12-18 19:46:38 -0300

    feat: update contentful.js

--- src/api/contentful.js
+++ src/api/contentful.js
@@ -2,8 +2,8 @@ const FAQ_GRAPHQL_FIELDS = `
 sys  {
   id 
 }
-question
 internalName
+question
 answer {
   json
 }
@@ -13,14 +13,99 @@ const CURIOSITIES_GRAPHQL_FIELDS = `
 sys  {
   id 
 }
-topic
 internalName
+topic
 content {
   json
 }
 `
 
-async function getGraphQLFields(contentType) {
+const ABOUT_ME_GRAPHQL_FIELDS = `
+sys {
+  id
+}
+title
+media {
+  imageDescription
+  media {
+    url
+  }
+}
+content {
+  content {
+    json
+  }
+}
+`
+const BLOG_POST_GRAPHQL_FIELDS = `
+  sys {
+    id
+  }
+  internalName
+  title
+  author
+  date
+  tag
+  project
+  slug
+  image {
+    url
+  }
+  contentCollection {
+    items {
+      content {
+        json
+      }
+      gallery {
+        __typename
+        ... on GalleryV1 {
+          sys {
+            id
+          }
+          mediaCollection {
+            items {
+              imageDescription
+              media {
+                url
+              }
+            }
+          }
+        }
+        ... on GalleryV2 {
+          sys {
+            id
+          }
+          mediaCollection {
+            items {
+              imageDescription
+              media {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const BLOG_POST_GRAPHQL_FIELDS_SUMMARY = `
+  sys {
+    id
+  }
+  internalName
+  title
+  author
+  date
+  tag
+  project
+  slug
+  image {
+    url
+  }
+`;
+
+async function getGraphQLFields(contentType, isSummary = false) {
   let graphqlFields
   switch (contentType) {
     case 'faq':
@@ -29,6 +114,12 @@ async function getGraphQLFields(contentType) {
     case 'curiosity':
       graphqlFields = CURIOSITIES_GRAPHQL_FIELDS
       break
+    case 'aboutMe':
+      graphqlFields = ABOUT_ME_GRAPHQL_FIELDS
+      break
+      case 'blogPost':
+        graphqlFields = isSummary ? BLOG_POST_GRAPHQL_FIELDS_SUMMARY : BLOG_POST_GRAPHQL_FIELDS;
+        break;
     default:
       break
   }
@@ -49,39 +140,42 @@ async function fetchGraphQL(query, preview = false) {
     headers.Authorization = `Bearer ${accessToken}`
   }
 
-  return fetch(
+  const response = await fetch(
     `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
     {
       method: 'POST',
       headers,
       body: JSON.stringify({ query }),
-      next: {
-        revalidate: 3600,
-      },
     }
-  ).then((response) => {
-    return response.json()
-  })
+  );
+
+  const data = await response.json();
+  return data;
 }
 
 function extractEntries(fetchResponse, contentType) {
   switch (contentType) {
     case 'faq':
-      return fetchResponse?.data?.faqCollection?.items;
+      return fetchResponse?.data?.faqCollection?.items || []
     case 'curiosity':
-      return fetchResponse?.data?.curiosityCollection?.items;
+      return fetchResponse?.data?.curiosityCollection?.items || []
+    case 'aboutMe':
+      return fetchResponse?.data.aboutMeCollection?.items || []
+    case 'blogPost':
+      return fetchResponse?.data.blogPostCollection?.items || []
     default:
-      return [];
+      return []
   }
 }
 
 export async function getAllEntries(
   contentType,
   limit = 100,
-  isDraftMode = false
+  isDraftMode = false,
+  isSummary = false
 ) {
   try {
-    const graphqlFields = await getGraphQLFields(contentType)
+    const graphqlFields = await getGraphQLFields(contentType, isSummary);
     const query = `query {
       ${contentType}Collection(
         limit: ${limit},
@@ -91,38 +185,43 @@ export async function getAllEntries(
           ${graphqlFields}
         }
       }
-    }`
+    }`;
 
-    const entries = await fetchGraphQL(query, isDraftMode)
-
-    return extractEntries(entries, contentType)
+    const entries = await fetchGraphQL(query, isDraftMode);
+    return extractEntries(entries, contentType);
   } catch (error) {
-    console.error('Erro ao buscar entradas:', error)
-    return []
+    console.error('Erro ao buscar entradas:', error);
+    return [];
   }
 }
 
-export async function getEntry(contentType, isDraftMode = false) {
+export async function getEntry(contentType, limit = 3, isDraftMode = false) {
   try {
     const graphqlFields = await getGraphQLFields(contentType)
     const entry = await fetchGraphQL(
       `query {
         ${contentType}Collection(
-          preview: ${isDraftMode ? "true" : "false"}
-          ) {
-            items {
-              ${graphqlFields}
-            }
+          limit: ${limit},
+          preview: ${isDraftMode ? 'true' : 'false'}
+        ) {
+          items {
+            ${graphqlFields}
           }
-        }`,
+        }
+      }`,
       isDraftMode
-    )
+    );
 
+    console.log('GraphQL Fields:', graphqlFields);
+    console.log('GraphQL Response:', entry);
+
+    const entryData = extractEntries(entry, contentType)[0];
     return {
-      ...extractEntries(entry, contentType)[0],
+      ...entryData,
     }
   } catch (error) {
     console.error('Erro ao buscar entrada:', error)
     return null
   }
 }
+

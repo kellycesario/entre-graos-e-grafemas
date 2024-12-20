e7c9b97de3123c3d970fa8f7ae145a237e7e2668
kellycesario <kellycesario@gmail.com>
2024-12-19 23:32:43 -0300

    feat: integrate 'recentVideosWrapper' with youtube + swr

--- src/components/organisms/RecentVideosWrapper/index.tsx
+++ src/components/organisms/RecentVideosWrapper/index.tsx
@@ -1,23 +1,54 @@
+'use client'
+import useSWR from 'swr'
+import axios from 'axios'
 import { v4 as uuidv4 } from 'uuid'
 import { IconArrowRight } from '@tabler/icons-react'
 import { LocalIcon } from '@/ions/LocalIcon'
 import { Heading } from '@/atoms/Heading'
 import { Button } from '@/atoms/Button'
 import { Text } from '@/atoms/Text'
-import { CardVideo } from '@/molecules/CardVideo'
-import recentVideosMock from '@/data/recentVideosMock.json'
+import { CardVideo, CardProps } from '@/molecules/CardVideo'
 import styles from './styles.module.scss'
 
 interface RecentVideosWrapperProps {
   title?: string
   text?: string
+  maxResults?: number
+}
+
+const fetchVideos = async (maxResults: number) => {
+  const API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY
+  const channelId = process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID
+  const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${channelId}&order=date&part=snippet&type=video&maxResults=${maxResults}`
+
+  const response = await axios.get(apiUrl)
+  return response.data.items.map((item: any) => ({
+    image: item.snippet.thumbnails.high.url,
+    title: item.snippet.title,
+    tag: 'Novo vídeo',
+    link: `https://www.youtube.com/watch?v=${item.id.videoId}`,
+  }))
 }
 
 export const RecentVideosWrapper = ({
   title,
   text,
+  maxResults = 5,
 }: RecentVideosWrapperProps) => {
   const id = uuidv4()
+
+  const { data: videos, error } = useSWR(
+    `youtube-videos-${maxResults}`,
+    () => fetchVideos(maxResults),
+    {
+      revalidateOnFocus: false,
+      refreshInterval: 43200000,
+      dedupingInterval: 60000,
+    }
+  )
+
+  if (error) return null
+  if (!videos) return null
 
   return (
     <section aria-labelledby={id} className={styles.recentVideos}>
@@ -29,15 +60,16 @@ export const RecentVideosWrapper = ({
           <Text color="white">{text}</Text>
         </div>
         <div className={styles.recentVideos__cards}>
-          {recentVideosMock.slice(0, 2).map(({ image, title, tag, link }) => (
-            <CardVideo
-              key={title}
-              image={image}
-              tag={tag}
-              title={title}
-              link={link}
-            />
-          ))}
+          {videos.length > 0 &&
+            videos.map(({ image, title, tag, link }: CardProps) => (
+              <CardVideo
+                key={title}
+                image={image}
+                tag={tag}
+                title={title}
+                link={link}
+              />
+            ))}
           <LocalIcon
             icon="arrows"
             width={101}
@@ -54,6 +86,7 @@ export const RecentVideosWrapper = ({
           secondaryColor="#FFF"
           label="Acesse o canal"
           variant="secondary"
+          className={styles.recentVideos__button}
         />
       </div>
     </section>

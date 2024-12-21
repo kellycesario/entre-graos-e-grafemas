44022bf30abf465547f7a5c2aec6554c3b52ed47
kellycesario <kellycesario@gmail.com>
2024-12-21 10:56:55 -0300

    feat: add footer at slugs pages

--- src/app/elinc/[slug]/page.tsx
+++ src/app/elinc/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { ArticleHero } from '@/organisms/ArticleHero'
 import { ArticleExpanded } from '@/organisms/ArticleExpanded'
+import { Footer } from '@/organisms/Footer'
 import { getArticle } from '@/api/contentful'
 import { extractImages, hasImages } from '@/api/contentfulGlobalFunctions'
 
@@ -12,35 +13,42 @@ export default async function Article({ params }: Readonly<ArticleProps>) {
   const items = article.contentCollection?.items || []
 
   return (
-    <main>
-      <ArticleHero
-        title={article.title}
-        author={article.author}
-        date={article.date}
-        image={article.image?.url}
-        alt={article.image?.imageDescription}
+    <>
+      <main>
+        <ArticleHero
+          title={article.title}
+          author={article.author}
+          date={article.date}
+          image={article.image?.url}
+          alt={article.imageDescription}
+        />
+
+        {items.map((item: any, index: number) => {
+          const content = item.content || {}
+          const galleryTypeName = item.gallery ? item.gallery.__typename : null
+          const imagesV1 =
+            galleryTypeName === 'GalleryV1' ? extractImages(item.gallery) : []
+          const imagesV2 =
+            galleryTypeName === 'GalleryV2' ? extractImages(item.gallery) : []
+
+          return (
+            <section key={index + 1}>
+              <ArticleExpanded
+                content={content}
+                imagesV1={imagesV1}
+                imagesV2={imagesV2}
+                hasImagesV1={hasImages(item.gallery)}
+                hasImagesV2={hasImages(item.gallery)}
+              />
+            </section>
+          )
+        })}
+      </main>
+      <Footer
+        hasCTA={true}
+        backgroundImage="/images/coffee/9.png"
+        backgroundColor="#8c4949"
       />
-
-      {items.map((item: any, index: number) => {
-        const content = item.content || {}
-        const galleryTypeName = item.gallery ? item.gallery.__typename : null
-        const imagesV1 =
-          galleryTypeName === 'GalleryV1' ? extractImages(item.gallery) : []
-        const imagesV2 =
-          galleryTypeName === 'GalleryV2' ? extractImages(item.gallery) : []
-
-        return (
-          <section key={index + 1}>
-            <ArticleExpanded
-              content={content}
-              imagesV1={imagesV1}
-              imagesV2={imagesV2}
-              hasImagesV1={hasImages(item.gallery)}
-              hasImagesV2={hasImages(item.gallery)}
-            />
-          </section>
-        )
-      })}
-    </main>
+    </>
   )
 }

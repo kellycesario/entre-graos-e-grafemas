0127208d551eb3c4987465203b8a0b59be120a87
kellycesario <kellycesario@gmail.com>
2024-12-29 01:01:22 -0300

    feat: update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,10 +13,6 @@ const config: StorybookConfig = {
     name: '@storybook/nextjs',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
   staticDirs: ['../public'],
 }
-
 export default config

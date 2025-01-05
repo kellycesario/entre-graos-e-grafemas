baed4432c454add4ca26878589531c596d924152
kellycesario <kellycesario@gmail.com>
2025-01-05 17:30:08 -0300

    fix: ReferenceError

--- src/components/molecules/CardVideo/index.tsx
+++ src/components/molecules/CardVideo/index.tsx
@@ -1,10 +1,10 @@
+import { useEffect, useState } from 'react'
 import { LargeButton } from '@/atoms/LargeButton'
 import { Tag } from '@/atoms/Tag'
 import styles from './styles.module.scss'
 
 export interface CardProps {
   image: { url: string }
-
   title?: string
   tag?: string
   link?: string
@@ -18,26 +18,30 @@ export const CardVideo = ({
   link,
   className,
 }: CardProps) => {
+  const [decodedTitle, setDecodedTitle] = useState<string>('')
+
+  useEffect(() => {
+    if (title) {
+      const txt = document.createElement('textarea')
+      txt.innerHTML = title
+      setDecodedTitle(txt.value)
+    }
+  }, [title])
+
   const cardStyle = [styles.card, className].join(' ')
 
-  const decodeHtml = (html: string) => {
-    const txt = document.createElement('textarea')
-    txt.innerHTML = html
-    return txt.value
-  }
-
   return (
-    <article className={cardStyle} aria-label={title}>
+    <article className={cardStyle} aria-label={decodedTitle}>
       <div
         className={styles.card__image}
         style={{ backgroundImage: `url(${image.url})` }}
       />
       <div className={styles.card__heading}>
         {tag && <Tag color="white">{tag}</Tag>}
-        <p className={styles.card__title}>{decodeHtml(title ?? '')}</p>
+        <p className={styles.card__title}>{decodedTitle}</p>
       </div>
       <div className={styles.card__player}>
-        <LargeButton link={link} title={title} />
+        <LargeButton link={link} title={decodedTitle} />
       </div>
     </article>
   )
